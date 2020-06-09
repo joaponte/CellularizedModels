9867fe6daf502887d737fc750bcebda0bc3f13ec
joaponte <josua.aponte@gmail.com>
2020-06-09 13:14:45 -0400

    Changed Names

--- CoInfection/Simulation/CoinfectionModel.py
+++ CoInfection/Simulation/CoinfectionModel.py
@@ -4,13 +4,13 @@ from cc3d import CompuCellSetup
 from AmberFluModelSteppables import AmberFluModelSteppable
 CompuCellSetup.register_steppable(steppable=AmberFluModelSteppable(frequency=1))
 
-from AmberFluModelSteppables import CellularModelSteppable
+from CoinfectionModelSteppables import CellularModelSteppable
 CompuCellSetup.register_steppable(steppable=CellularModelSteppable(frequency=1))
 
-from AmberFluModelSteppables import Data_OutputSteppable
+from CoinfectionModelSteppables import Data_OutputSteppable
 CompuCellSetup.register_steppable(steppable=Data_OutputSteppable(frequency=1))
 
-from AmberFluModelSteppables import PlotSteppable
+from CoinfectionModelSteppables import PlotSteppable
 CompuCellSetup.register_steppable(steppable=PlotSteppable(frequency=1))
 
 CompuCellSetup.run()
