cfbc38e0b228fc94f38e8759673b0b5de77573e1
joaponte <josua.aponte@gmail.com>
2020-06-10 03:28:11 -0400

    First Commit

---
+++ TarunsModel/Simulation/TarunsModel.py
@@ -0,0 +1,10 @@
+
+from cc3d import CompuCellSetup
+
+from TarunsModelSteppables import TarunsModelSteppable
+CompuCellSetup.register_steppable(steppable=TarunsModelSteppable(frequency=1))
+
+from TarunsModelSteppables import PlotsSteppable
+CompuCellSetup.register_steppable(steppable=PlotsSteppable(frequency=1))
+
+CompuCellSetup.run()
